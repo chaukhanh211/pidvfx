4c18bbb56f30c6270a8e38b297709251a4fa8451
daokhanhchau <daokhanhchau211@gmail.com>
2021-05-03 18:10:46 +0700

    fix

--- script/controller.js
+++ script/controller.js
@@ -16,52 +16,52 @@ const app = {
         {
           name: "WAVVE 24시",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/ncmK-LsrRJY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/WAVVE.jpg"
+          image: "imgThumb/WAVVE.jpg"
         },
         {
           name: "Tiger remix",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/_8-XPTglbUM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/tiger.jpg"
+          image: "imgThumb/tiger.jpg"
         },
         {
           name: "Huyền Thoại RUNETERRA",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/EsLCWt0A8ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/Huyen-thoai-RUNETERRA.jpg"
+          image: "imgThumb/Huyen-thoai-RUNETERRA.jpg"
         },
         {
           name: "NƯỚC TĂNG LỰC NUMBER1 CHANH & DÂU",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/qQi_zn_zOJE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/number1.jpg"
+          image: "imgThumb/number1.jpg"
         },
         {
           name: "Tuyết Ưng VNG",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/iAz5VOqRfNw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/tuyetung.jpg"
+          image: "imgThumb/tuyetung.jpg"
         },
         {
           name: "TVC KR",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/YGcgghFRpoI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/tvc-kr.jpg"
+          image: "imgThumb/tvc-kr.jpg"
         },
         {
           name: "showreel 2019 - 2020",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/4iM9tVGSyrU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/showreel.jpg"
+          image: "imgThumb/showreel.jpg"
         },
         {
           name: "Rigging Training",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/sK81mj_tFLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/Riggingtraining.jpg"
+          image: "imgThumb/Riggingtraining.jpg"
         },
         {
           name: "Animation Training",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/ZZntlpXbLvc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/animationtraining.jpg"
+          image: "imgThumb/animationtraining.jpg"
         },
         {
           name: "Showreel VFX 2018",
           path: '<iframe width="830" height="500" src="https://www.youtube.com/embed/TzPhvU5cySs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-          image: "/Web/imgThumb/showreel2018.jpg"
+          image: "imgThumb/showreel2018.jpg"
         },
         
     ],
